8675e452a0de6f180e53b916e61bcf2387e9c124
Eshan <eshan1285@gmail.com>
2020-05-19 18:05:20 +0530

    Updates

---
+++ precache-manifest.203f1da6750dd30bcb2fefac4d0a3004.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de10c3d5856197ae570ba373dcb3e61",
+    "url": "/bites-beverages-web/index.html"
+  },
+  {
+    "revision": "a2a069ea01855bb657df",
+    "url": "/bites-beverages-web/static/css/2.64c6657a.chunk.css"
+  },
+  {
+    "revision": "43a65cd1b01b3cdcea33",
+    "url": "/bites-beverages-web/static/css/main.8cc471ad.chunk.css"
+  },
+  {
+    "revision": "a2a069ea01855bb657df",
+    "url": "/bites-beverages-web/static/js/2.38814cc2.chunk.js"
+  },
+  {
+    "revision": "bec29b214d35d644a6cd85d9d738c742",
+    "url": "/bites-beverages-web/static/js/2.38814cc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a65cd1b01b3cdcea33",
+    "url": "/bites-beverages-web/static/js/main.69ee5c96.chunk.js"
+  },
+  {
+    "revision": "a63d3f06d47f1a0de451",
+    "url": "/bites-beverages-web/static/js/runtime-main.1d4cf9bd.js"
+  },
+  {
+    "revision": "763746e5260e73a78f402c9e5c6e7a89",
+    "url": "/bites-beverages-web/static/media/circle-food-cropped.763746e5.jpg"
+  },
+  {
+    "revision": "b1a5166a028be0464137d8c730974d0c",
+    "url": "/bites-beverages-web/static/media/circle-food.b1a5166a.jpg"
+  },
+  {
+    "revision": "c2198a24b6a9227548840270230a9e00",
+    "url": "/bites-beverages-web/static/media/cooking.c2198a24.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/bites-beverages-web/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/bites-beverages-web/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/bites-beverages-web/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/bites-beverages-web/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/bites-beverages-web/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "02e8fab67c9752bbb3f69754b4e2b833",
+    "url": "/bites-beverages-web/static/media/food10.02e8fab6.jpg"
+  },
+  {
+    "revision": "476a1352c9e804f5d66980c7eb9893f1",
+    "url": "/bites-beverages-web/static/media/food11.476a1352.jpg"
+  },
+  {
+    "revision": "a2093d5dc86ce87048b32f366f77071e",
+    "url": "/bites-beverages-web/static/media/food13.a2093d5d.jpg"
+  },
+  {
+    "revision": "4461fa6d2b56ebbe2d7cf21718ebf326",
+    "url": "/bites-beverages-web/static/media/food6.4461fa6d.jpg"
+  },
+  {
+    "revision": "0db406413080e95861d1fdb5bd5bcd1f",
+    "url": "/bites-beverages-web/static/media/food7.0db40641.jpg"
+  },
+  {
+    "revision": "bcf7a61f52165dbaa996c33ec15a1a7f",
+    "url": "/bites-beverages-web/static/media/food8.bcf7a61f.jpg"
+  },
+  {
+    "revision": "a88d88e6416bb2e87acbad21ce77410c",
+    "url": "/bites-beverages-web/static/media/home-panel-11-1-1.a88d88e6.jpg"
+  },
+  {
+    "revision": "2c6d5b0742eb0f6c03ffbf161048df10",
+    "url": "/bites-beverages-web/static/media/left-food.2c6d5b07.jpg"
+  }
+]);
